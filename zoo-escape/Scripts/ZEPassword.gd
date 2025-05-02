85e5836d6ddeaa124ade4699492292685e48eec2
asperkraken <gusyoung82@gmail.com>
2025-05-02 18:13:26 -0500

    - ui upgrades to password menu
    - sound effects added
    - buttons vs hard coding for better integration across scenes
    - back function to return to menu
    - clearing per unit function
    - code rebuilt

--- zoo-escape/Scripts/ZEPassword.gd
+++ zoo-escape/Scripts/ZEPassword.gd
@@ -1,40 +1,42 @@
-extends Node2D
+extends Control
 
-@onready var Pos1 := Vector2(272, 160)
-@onready var Pos2 := Vector2(304, 160)
-@onready var Pos3 := Vector2(336, 160)
-@onready var Pos4 := Vector2(272, 184)
-@onready var Pos5 := Vector2(304, 184)
-@onready var Pos6 := Vector2(336, 184)
-@onready var Pos7 := Vector2(272, 208)
-@onready var Pos8 := Vector2(304, 208)
-@onready var Pos9 := Vector2(336, 208)
-@onready var PosClear := Vector2(272, 232)
-@onready var Pos0 := Vector2(304, 232)
-@onready var PosEnter := Vector2(336, 232)
-@onready var buttonMatrix := [[Pos1, Pos2, Pos3], [Pos4, Pos5, Pos6], [Pos7, Pos8, Pos9], [PosClear, Pos0, PosEnter]]
-@onready var cursorPos := Vector2i.ZERO
-@onready var codeTextPos := 0
+enum NUMBER_FOCUS_STATES {
+	ZERO,
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	CLEAR,
+	ENTER}
+var numberFocusState : int = 1
 
-@onready var selector := $Selector
 @onready var code := $Code
+var codeTextPos : int = 0
+
 @export var inGameMode : bool = false ## used to determine behavior in-game
 var windowOpenFlag : bool = false ## flag for checking if window is open
 var inputBufferActive : bool = true ## hold input until window fades in
+@export var loadSceneBufferTime : int = 1
 
 
 ## materials for shader changes on password entry
 const correctShader = preload("res://Assets/Shaders/wobbly_material.tres")
 const failShader = preload("res://Assets/Shaders/error_shake_x.tres")
+const title = "res://Scenes/ZETitle.tscn"
 const empty = "----"
 
 
 func _ready() -> void:
-	selector.position = buttonMatrix[cursorPos.y][cursorPos.x]
-	code.text = "----"
+	code.text = empty
 	if !inGameMode:
 		$Animator.play("fade_in")
-		$BufferTimer.start()
+		$InputBufferTimer.start()
+		$ButtonBox/Button1.grab_focus()
 
 
 func _input(_event: InputEvent) -> void:
@@ -57,107 +59,112 @@ func _input(_event: InputEvent) -> void:
 		fetchInput()
 
 
-func fetchInput():
+func randomBlipCue():
 	var _variant = randf_range(-0.7,0.7) ## random blips
 	SoundControl.playCue(SoundControl.blip,(3.0+_variant))
-	if Input.is_action_just_pressed("ui_cancel"):
+
+
+func fetchInput():
+	if Input.is_action_just_pressed("PasswordButton"):
 		SoundControl.playCue(SoundControl.down,2.0)
 		if !inGameMode:
-			SceneManager.GoToNewSceneString(self, Scenes.ZETitle)
+			returnToTitle()
+		else:
+			$Animator.play_backwards("fade_in")
+			code.text = empty
+			codeTextPos = 0
+			inputBufferActive = true
+			windowOpenFlag = false
 
 		
 	if Input.is_action_just_pressed("ActionButton"):
-		if selector.position == PosClear:
-			code.text = "----"
-			codeTextPos = 0
-		elif selector.position == PosEnter:
+		if numberFocusState == NUMBER_FOCUS_STATES.ENTER:
 			answerCheck()
-		else:
-			if code.text.contains("-"):
-				SetNum()
-				
-	if Input.is_action_just_pressed("CancelButton"):
-		if codeTextPos != 0:
-			codeTextPos -= 1
-			code.text[codeTextPos] = "-"
-	
-	if Input.is_action_just_pressed("DigitalUp"):
-		if cursorPos.y == 0:
-			cursorPos.y = 3
-		else:
-			cursorPos.y -= 1
-		
-	if Input.is_action_just_pressed("DigitalRight"):
-		if cursorPos.x == 2:
-			cursorPos.x = 0
-		else:
-			cursorPos.x += 1
-		
-	if Input.is_action_just_pressed("DigitalDown"):
-		if cursorPos.y == 3:
-			cursorPos.y = 0
-		else:
-			cursorPos.y += 1
 			
-	if Input.is_action_just_pressed("DigitalLeft"):
-		if cursorPos.x == 0:
-			cursorPos.x = 2
+		if Globals.Game_Globals.has(code.text):
+			SoundControl.playCue(SoundControl.success,2.5)
+			$Code.material = correctShader
+			$Code.modulate = Color.GREEN_YELLOW
+			$LoadSceneBuffer.start(loadSceneBufferTime)
+
+
+		if !"-" in code.text:
+			numberFocusState = NUMBER_FOCUS_STATES.ENTER
+			$ButtonBox/ButtonEnter.grab_focus()
+
+
+	if Input.is_action_just_pressed("CancelButton"):
+		if codeTextPos > -1:
+			numberFocusState = NUMBER_FOCUS_STATES.CLEAR
+			$ButtonBox/ButtonClear.grab_focus()
+			codeRemoval()
 		else:
-			cursorPos.x -= 1
-	
-	selector.position = buttonMatrix[cursorPos.y][cursorPos.x]
-
-	if Input.is_action_just_released("ActionButton") and Globals.Game_Globals.has(code.text):
-		SoundControl.playCue(SoundControl.success,2.5)
-		$Code.material = correctShader
-		$Code.modulate = Color.GREEN_YELLOW
+			returnToTitle()
 
 
-	if Input.is_action_just_released("ActionButton"):
-		if codeTextPos == 4:
-			selector.position = PosEnter
+func codeRemoval():
+	if codeTextPos != 0:
+		codeTextPos -= 1
+		code.text[codeTextPos] = "-"
+	if codeTextPos <= 0:
+		codeTextPos = 0
+		code.text = empty
+
+
+func returnToTitle():
+	SoundControl.playCue(SoundControl.down,1.4)
+	SceneManager.call_deferred("GoToNewSceneString",self, title)
 
 
 func answerCheck():
-	selector.position = PosEnter
 	if !code.text.contains("-") and Globals.Game_Globals.has(code.text):
-		SceneManager.call_deferred("GoToNewSceneString",self, Globals.Game_Globals[code.text])
+		$Code.material = correctShader
+		$Code.modulate = Color.GREEN_YELLOW
+		SoundControl.playCue(SoundControl.success,1.5)
+		$LoadSceneBuffer.start(1)
 	else:
-		selector.position = Pos0
 		codeTextPos = 0
 		$Code.text = "XXXX"
 		$Code.material = failShader
 		$Code.modulate = Color.CRIMSON
 		SoundControl.playCue(SoundControl.down,1.5)
-		$EffectTimer.start(0.5)
+		$TextEffectTimer.start(0.5)
+
+
+func _on_load_scene_buffer_timeout() -> void:
+	SceneManager.call_deferred("GoToNewSceneString",self, Globals.Game_Globals[code.text])
+
 
 ## if there are dashes, accept input
 func SetNum():
+	randomBlipCue()
 	var num := ""
-	match buttonMatrix[cursorPos.y][cursorPos.x]:
-		Pos0:
+	match numberFocusState:
+		0:
 			num = "0"
-		Pos1:
+		1:
 			num = "1"
-		Pos2:
+		2:
 			num = "2"
-		Pos3:
+		3:
 			num = "3"
-		Pos4:
+		4:
 			num = "4"
-		Pos5:
+		5:
 			num = "5"
-		Pos6:
+		6:
 			num = "6"
-		Pos7:
+		7:
 			num = "7"
-		Pos8:
+		8:
 			num = "8"
-		Pos9:
+		9:
 			num = "9"
 	
-	code.text[codeTextPos] = num
-	codeTextPos += 1
+	if codeTextPos < 4:
+		code.text[codeTextPos] = num
+		codeTextPos += 1
+
 
 
 ## this effect resets the password box to default material and style
@@ -171,3 +178,150 @@ func _on_effect_timer_timeout() -> void:
 ## turns off input buffer, timer runs on window open
 func _on_buffer_timer_timeout() -> void:
 	inputBufferActive = false
+
+
+func _on_button_1_pressed() -> void:
+	SetNum()
+
+
+func _on_button_1_focus_entered() -> void:
+	numberFocusState = 1
+
+
+func _on_button_1_mouse_entered() -> void:
+	numberFocusState = 1
+
+
+func _on_button_2_pressed() -> void:
+	SetNum()
+
+
+func _on_button_2_focus_entered() -> void:
+	numberFocusState = 2
+
+
+func _on_button_2_mouse_entered() -> void:
+	numberFocusState = 2
+
+
+func _on_button_3_pressed() -> void:
+	SetNum()
+
+
+func _on_button_3_focus_entered() -> void:
+	numberFocusState = 3
+
+
+func _on_button_3_mouse_entered() -> void:
+	numberFocusState = 3
+
+
+func _on_button_4_pressed() -> void:
+	SetNum()
+
+
+func _on_button_4_focus_entered() -> void:
+	numberFocusState = 4
+
+
+func _on_button_4_mouse_entered() -> void:
+	numberFocusState = 4
+
+
+func _on_button_5_pressed() -> void:
+	SetNum()
+
+
+func _on_button_5_focus_entered() -> void:
+	numberFocusState = 5
+
+
+func _on_button_5_mouse_entered() -> void:
+	numberFocusState = 5
+
+
+func _on_button_6_pressed() -> void:
+	SetNum()
+
+
+func _on_button_6_focus_entered() -> void:
+	numberFocusState = 6
+
+
+func _on_button_6_mouse_entered() -> void:
+	numberFocusState = 6
+
+
+func _on_button_7_pressed() -> void:
+	SetNum()
+
+
+func _on_button_7_focus_entered() -> void:
+	numberFocusState = 7
+
+
+func _on_button_7_mouse_entered() -> void:
+	numberFocusState = 7
+
+
+func _on_button_8_pressed() -> void:
+	SetNum()
+
+
+func _on_button_8_focus_entered() -> void:
+	numberFocusState = 8
+
+
+func _on_button_8_mouse_entered() -> void:
+	numberFocusState = 8
+
+
+func _on_button_9_pressed() -> void:
+	SetNum()
+
+
+func _on_button_9_focus_entered() -> void:
+	numberFocusState = 9
+
+
+func _on_button_9_mouse_entered() -> void:
+	numberFocusState = 9
+
+
+func _on_button_0_pressed() -> void:
+	SetNum()
+
+
+func _on_button_0_focus_entered() -> void:
+	numberFocusState = 0
+
+
+func _on_button_0_mouse_entered() -> void:
+	numberFocusState = 0
+
+
+func _on_button_clear_pressed() -> void:
+	if codeTextPos > 0:
+		codeRemoval()
+	else:
+		returnToTitle()
+
+
+func _on_button_clear_focus_entered() -> void:
+	numberFocusState = 10
+
+
+func _on_button_clear_mouse_entered() -> void:
+	numberFocusState = 10
+
+
+func _on_button_enter_pressed() -> void:
+	answerCheck()
+
+
+func _on_button_enter_focus_entered() -> void:
+	numberFocusState = 11
+
+
+func _on_button_enter_mouse_entered() -> void:
+	numberFocusState = 11
