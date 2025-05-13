ac79a9d9f7a2f342be21648c5198aad29800fa25
DevMichaelV <devmvogel@gmail.com>
2025-05-13 17:45:41 -0500

    Update Scenes.gd to contain constant string values for scenes
    All references to different scenes should be pulling from the Scenes global.
    
    To do that, all scenes listed in scenes.gd need to be constant values.  They will never change while the game is running.  They are constant.
    
    - Updated scenes.gd and ZEPassword.gd to reflect that for this PR.  Game no longer crashes when returning to ZETitle from ZEPassword.

--- zoo-escape/GlobalScripts/scenes.gd
+++ zoo-escape/GlobalScripts/scenes.gd
@@ -1,13 +1,17 @@
 extends Node
 
 # This global will keep track of Scene names for easy switching
-@onready var GameRootScene := "res://Scenes/Core/GameRoot.tscn"
+const GameRootScene := "res://Scenes/Core/GameRoot.tscn"
 
-# Panther Escape Scenes 
-@onready var ZETitle := "res://Scenes/Core/ZETitle.tscn"
-@onready var ZEPassword := "res://Scenes/Core/ZEPassword.tscn"
-@onready var ZEDebug := "res://Scenes/Levels/ZEDebug.tscn"
-@onready var ZEDebug2 := "res://Scenes/Levels/ZEDebug2.tscn"
-@onready var ZETutorial1 := "res://Scenes/Levels/ZETutorial1.tscn"
-@onready var ZELevel1 := "res://Scenes/Levels/ZELevel1.tscn"
-@onready var ZESettings := "res://Scenes/Core/ZESettings.tscn"
+# Zoo Escape Core Scenes
+const ZETitle := "res://Scenes/Core/ZETitle.tscn"
+const ZESettings := "res://Scenes/Core/ZESettings.tscn"
+const ZEPassword := "res://Scenes/Core/ZEPassword.tscn"
+
+# Zoo Escape Debug Level Scenes
+const ZEDebug := "res://Scenes/Levels/ZEDebug.tscn"
+const ZEDebug2 := "res://Scenes/Levels/ZEDebug2.tscn"
+
+# Zoo Escape Production Level Scenes
+const ZETutorial1 := "res://Scenes/Levels/ZETutorial1.tscn"
+const ZELevel1 := "res://Scenes/Levels/ZELevel1.tscn"
