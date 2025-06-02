895e36c7689f426877f99d8a17a49a4e70422cf0
asperkraken <gusyoung82@gmail.com>
2025-06-01 21:15:07 -0500

    - add no layer graphics for sand water tiles

--- zoo-escape/Scripts/Components/Interactive/Box.gd
+++ zoo-escape/Scripts/Components/Interactive/Box.gd
@@ -48,7 +48,9 @@ func bodyEnter(body: Node2D) -> void:
 	if body is TileMapLayer:
 		var tilePos: Vector2i = body.local_to_map($GroundCheck.global_position)
 		if body.get_cell_tile_data(body.local_to_map($GroundCheck.global_position)).get_custom_data("Water"):
-			body.set_cell(body.local_to_map($GroundCheck.global_position), 2, body.get_cell_atlas_coords(body.local_to_map($GroundCheck.global_position)))
+			# check old id before updating to new to ensure correct tilemap set
+			var _oldSourceId = body.get_cell_source_id(body.local_to_map($GroundCheck.global_position))
+			body.set_cell(body.local_to_map($GroundCheck.global_position), int(_oldSourceId+1), body.get_cell_atlas_coords(body.local_to_map($GroundCheck.global_position)))
 			SoundControl.playSfx(SoundControl.splorch)
 			currentState = states.INWATER
 			collision_layer = 0
